f59b69a0d0ed6314c8426e7f0f350050d356959e
SandraMireles <mirelesa4@gmail.com>
2018-12-10 18:36:56 -0600

    Version_2 Luminosidad habitacion principal mediante sensor ldr

--- Prototipo_SmartHouse_Codigo/Prototipo_SmartHouse_Codigo.ino
+++ Prototipo_SmartHouse_Codigo/Prototipo_SmartHouse_Codigo.ino
@@ -1,12 +1,27 @@
-  int HABITACION = 13; // Pin digital para el LED
+
+  /*HABITACION*/
+  int HABITACION = 13; // Pin digital para el LED  
   char encendido_habitacion; // Variable para guardar el valor del carácter enviado
+
+  /*LUZ PRINCIPAL CONTROLADA POR LDR*/
+   int principal = 12;
+   int sensorReading;//Pin análogo en espera
+  
    
   void setup()  {
-    pinMode(HABITACION, OUTPUT); // Pin digital del LED como salida.
+    
     Serial.begin(9600); // Velocidad de comunicación en baudios.
+    pinMode(HABITACION, OUTPUT); // Pin digital del LED como salida.
+    pinMode(principal,OUTPUT);  // habitacion principal
+
+  }
+
+  void loop()  {
+    Habitacion();
+    Principal ();
   }
    
-  void loop()  {
+  void Habitacion ()  {
     
     if (Serial.available() > 0) // Si se recibe un carácter...
     {
@@ -23,8 +38,18 @@
       {
         digitalWrite(HABITACION, LOW); // Se apaga el LED    
        // Serial.write(encendido_habitacion); // verfica valor recibido
-        Serial.print(" Luz de habitacion apagada"); //estado de la habitacion
+        Serial.print("Luz de habitacion apagada"); //estado de la habitacion
         Serial.print("\n");
       }
     }
   }
+
+  void Principal(){
+    sensorReading=analogRead(0); //Instrucción para obtener dato analógico
+      if (sensorReading<200) {
+        digitalWrite(principal,HIGH);
+    } else 
+        digitalWrite(principal,LOW);
+        Serial.println(sensorReading); 
+  }
+
